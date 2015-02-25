1f3bef7ba42d0067376d67d90db1cad1ad3eb299
ParlData <visegrad@parldata.eu>
2015-02-25 16:03:22 +0000

    fixed python path and removed cz_psp subdomain

--- subdomains/cz_psp/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-_, parl = os.path.dirname(__file__).rsplit('/', 1)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "subdomains.%s.settings" % parl)
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
