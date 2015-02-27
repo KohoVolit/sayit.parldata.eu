4b98f5085fa2aaecc5de3e2e3c6d26a573202303
ParlData <visegrad@parldata.eu>
2015-02-27 18:41:15 +0000

    fixed ordering of sections

--- sayit_parldata_eu/views.py
+++ sayit_parldata_eu/views.py
@@ -8,11 +8,11 @@ class RecentView(ParentlessList):
         context = super(ParentlessList, self).get_context_data(**kwargs)
 
         qs = Section.objects.for_instance(self.request.instance)
-        context['term_list'] = qs.filter(parent=None).reverse()[:]
+        context['term_list'] = qs.filter(parent=None).order_by('-start_date', '-start_time')[:]
         if context['term_list']:
             current_term = context['term_list'][0]
-            context['session_list'] = qs.filter(parent=current_term).reverse()[:5]
+            context['session_list'] = qs.filter(parent=current_term).order_by('-start_date', '-start_time')[:5]
             if context['session_list']:
                 current_session = context['session_list'][0]
-                context['sitting_list'] = qs.filter(parent=current_session).reverse()
+                context['sitting_list'] = qs.filter(parent=current_session).order_by('-start_date', '-start_time')
         return context
