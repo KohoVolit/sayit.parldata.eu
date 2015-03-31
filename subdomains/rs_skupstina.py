63f8b292513c36bf8a6fa0959b3a489362068bcd
ParlData <visegrad@parldata.eu>
2015-03-31 20:53:46 +0000

    configured fulltext search for Serbia

--- subdomains/rs_skupstina.py
+++ subdomains/rs_skupstina.py
@@ -4,12 +4,39 @@ COUNTRY_CODE = 'rs'
 PARLIAMENT_CODE = 'skupstina'
 PARLIAMENT_NAME = 'Народна скупштина Републике Србије'
 LANGUAGE_CODE = 'sr'
-#GA_PROPERTY_ID = '??'
+GA_PROPERTY_ID = '22'
 
-ELASTICSEARCH_ANALYZERS = {}
+# requires ICU Analysis and LemmaGen plugins for ElasticSearch
+# https://github.com/elastic/elasticsearch-analysis-icu
+# https://github.com/vhyza/elasticsearch-analysis-lemmagen
+ELASTICSEARCH_ANALYZERS = {
+    'serbian_lemmagen': {
+        'type': 'custom',
+        'tokenizer': 'standard',
+        'filter': ['cyrillic-latin', 'stopwords_sr', 'lemmagen_sr', 'lowercase', 'stopwords_sr', 'remove_duplicities'],
+    },
+}
 ELASTICSEARCH_TOKENIZERS = {}
-ELASTICSEARCH_FILTERS = {}
-# ELASTICSEARCH_USE_ANALYZER = '??'
+ELASTICSEARCH_FILTERS = {
+    'cyrillic-latin': {
+        'type': 'icu_transform',
+        'id': 'Cyrillic-Latin'
+    },
+    'stopwords_sr': {
+        'type': 'stop',
+        'stopwords_path': 'sr/sr.stop',
+        'ignore_case': True
+    },
+    'lemmagen_sr': {
+        'type': 'lemmagen',
+        'lexicon': 'sr'
+    },
+    'remove_duplicities': {
+        'type': 'unique',
+        'only_on_same_position': True
+    },
+}
+ELASTICSEARCH_USE_ANALYZER = 'serbian_lemmagen'
 
 
 # load project settings while respecting the above
